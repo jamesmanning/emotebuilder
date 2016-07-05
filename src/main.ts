e8106246dc4ac964a32b1c8ec6df4a969b9c14be
James Manning <james.manning@gmail.com>
2016-07-05 09:49:42 -0400

    remove bootstrap support for angular/forms since no longer using it

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
@@ -9,8 +8,6 @@ if (environment.production) {
 }
 
 bootstrap(AppComponent, [
-  disableDeprecatedForms(),
-  provideForms(),
   HTTP_PROVIDERS
 ]);
 
