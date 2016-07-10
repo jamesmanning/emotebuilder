d786c8382356cb5ab8a4cf689c45d09d474be5a5
James Manning <james.manning@gmail.com>
2016-07-09 22:24:41 -0400

    add router providers to main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
-import { AppComponent, environment } from './app/';
+
+import { AppComponent, environment, APP_ROUTER_PROVIDERS } from './app/';
 
 if (environment.production) {
   enableProdMode();
@@ -11,6 +12,7 @@ if (environment.production) {
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
   provideForms(),
-  HTTP_PROVIDERS
+  HTTP_PROVIDERS,
+  APP_ROUTER_PROVIDERS,
 ]);
 
