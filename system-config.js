0495b7b05d60235641d971ea9e2dcc88126c629b
James Manning <james.manning@gmail.com>
2016-07-15 19:56:25 -0400

    checksum update

--- system-config.js
+++ system-config.js
@@ -53,4 +53,4 @@ System.config({
 });
 // Apply the user's configuration.
 System.config({ map: map, packages: packages });
-//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-Bfqln6oT.tmp\0\src\system-config.js.map
+//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-T5RLje5W.tmp\0\src\system-config.js.map
