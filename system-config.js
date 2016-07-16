40ab9ac57ae3669d4bef5722197398a8776cbac9
James Manning <james.manning@gmail.com>
2016-07-15 20:26:20 -0400

    new version for gh-pages

--- system-config.js
+++ system-config.js
@@ -53,4 +53,4 @@ System.config({
 });
 // Apply the user's configuration.
 System.config({ map: map, packages: packages });
-//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-MR7Ob3rH.tmp\0\src\system-config.js.map
+//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-jR2mU8Iv.tmp\0\src\system-config.js.map
