69d95d059ca8bda5453058f2f790f5c6ed39174d
James Manning <james.manning@gmail.com>
2018-04-03 19:51:00 -0400

    adopt the existing form html to use bootstrap styling

--- src/EmoteForm.tsx
+++ src/EmoteForm.tsx
@@ -28,165 +28,231 @@ export class EmoteForm extends React.Component<EmoteFormProps, {}> {
 
     render() {
         return (
-            <div>
-                <label>
-                    emoteIdentifier:
-                    <input
-                        type="text"
-                        value={this.props.emoteObject.emoteIdentifier}
-                        onChange={this.props.emoteIdentifierChangeHandler}
-                    />
-                </label>
-                <br />
-                <label>
-                    slide:
-                    <input
-                        type="checkbox"
-                        checked={this.props.emoteObject.slide}
-                        onChange={this.props.slideChangeHandler}
-                    />
-                </label>
-                {!this.props.emoteObject.slide ? '' :
-                    <label>
-                        speed:
-                        <select defaultValue={this.props.emoteObject.speed} onChange={this.props.speedChangeHandler}>
-                            <option value="">-- optional slide speed modifier --</option>
-                            <option value="slowest">slowest</option>
-                            <option value="slower">slower</option>
-                            <option value="slow">slow</option>
-                            <option value="fast">fast</option>
-                            <option value="faster">faster</option>
-                            <option value="fastest">fastest</option>
-                        </select>
-                    </label>
-                }
-                <br />
+            <form className="form-horizontal">
+                <fieldset>
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="emoteName">Emote Name</label>
+                        <div className="col-md-8">
+                            <input
+                                id="emoteName"
+                                name="emoteName"
+                                className="form-control input-md"
+                                type="text"
+                                value={this.props.emoteObject.emoteIdentifier}
+                                onChange={this.props.emoteIdentifierChangeHandler}
+                                placeholder="emote name (bpsign, ajlie, etc)"
+                            />
+                        </div>
+                    </div>
 
-                <label>
-                    vibrate:
-                    <input
-                        type="checkbox"
-                        checked={this.props.emoteObject.vibrate}
-                        onChange={this.props.vibrateChangeHandler}
-                    />
-                </label>
-                <br />
-                <label>
-                    reverse:
-                    <input 
-                        type="checkbox" 
-                        checked={this.props.emoteObject.reverse} 
-                        onChange={this.props.reverseChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    hueRotate:
-                    <input 
-                        type="checkbox" 
-                        checked={this.props.emoteObject.hueRotate} 
-                        onChange={this.props.hueRotateChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    invertColors:
-                    <input 
-                        type="checkbox" 
-                        checked={this.props.emoteObject.invertColors} 
-                        onChange={this.props.invertColorsChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    spin:
-                    <select defaultValue={this.props.emoteObject.spin} onChange={this.props.spinChangeHandler}>
-                        <option value="">-- optional spin --</option>
-                        <option value="xspin">spin clockwise around x axis</option>
-                        <option value="yspin">spin clockwise around y axis</option>
-                        <option value="zspin">spin clockwise around z axis</option>
-                        <option value="spin">spin clockwise around all 3 axes</option>
-                        <option value="!xspin">spin counterclockwise around x axis</option>
-                        <option value="!yspin">spin counterclockwise around y axis</option>
-                        <option value="!zspin">spin counterclockwise around z axis</option>
-                        <option value="!spin">spin counterclockwise around all 3 axes</option>
-                    </select>
-                </label>
-                <br />
-                <label>
-                    rotateDegrees:
-                    <input
-                        type="number"
-                        min="0" 
-                        max="359" 
-                        step="1"
-                        value={this.props.emoteObject.rotateDegrees.toString()}
-                        onChange={this.props.rotateDegreesChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    brody: 
-                    <input 
-                        type="checkbox" 
-                        checked={this.props.emoteObject.brody} 
-                        onChange={this.props.brodyChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    xAxisTranspose:
-                    <input
-                        type="number"
-                        min="-150" 
-                        max="150" 
-                        step="1"
-                        value={this.props.emoteObject.xAxisTranspose.toString()}
-                        onChange={this.props.xAxisTransposeChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    zAxisTranspose:
-                    <input
-                        type="number"
-                        min="0" 
-                        max="10" 
-                        step="1"
-                        value={this.props.emoteObject.zAxisTranspose.toString()}
-                        onChange={this.props.zAxisTransposeChangeHandler} 
-                    />
-                </label>
-                <br />
-                <label>
-                    firstLineText: 
-                    <input 
-                        type="text" 
-                        value={this.props.emoteObject.firstLineText} 
-                        onChange={this.props.firstLineTextChangeHandler} 
-                    />
-                    </label>
-                    <br />
-                <label>
-                    secondLineText: 
-                    <input 
-                        type="text" 
-                        value={this.props.emoteObject.secondLineText} 
-                        onChange={this.props.secondLineTextChangeHandler} 
-                    />
-                    </label>
-                    <br />
-                <label>
-                    altText: 
-                    <input 
-                        type="text" 
-                        value={this.props.emoteObject.altText} 
-                        onChange={this.props.altTextChangeHandler} 
-                    />
-                    </label>
-                    <br />
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="firstLineText">First Line of Text</label>
+                        <div className="col-md-8">
+                            <input
+                                id="firstLineText"
+                                name="firstLineText"
+                                className="form-control input-md"
+                                type="text"
+                                value={this.props.emoteObject.firstLineText}
+                                onChange={this.props.firstLineTextChangeHandler}
+                                placeholder="first line of text"
+                            />
+                        </div>
+                    </div>
 
-            </div>
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="secondLineText">Second Line of Text</label>
+                        <div className="col-md-8">
+                            <input
+                                id="secondLineText"
+                                name="secondLineText"
+                                className="form-control input-md"
+                                type="text"
+                                value={this.props.emoteObject.secondLineText}
+                                onChange={this.props.secondLineTextChangeHandler}
+                                placeholder="second line of text"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="altText">Regular Alt Text</label>
+                        <div className="col-md-8">
+                            <input
+                                id="altText"
+                                name="altText"
+                                className="form-control input-md"
+                                type="text"
+                                value={this.props.emoteObject.altText}
+                                onChange={this.props.altTextChangeHandler}
+                                placeholder="regular alt text"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="checkboxes">Optional modifiers</label>
+                        <div className="col-md-2">
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.vibrate}
+                                        onChange={this.props.vibrateChangeHandler}
+                                    />
+                                    vibrate
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.reverse}
+                                        onChange={this.props.reverseChangeHandler}
+                                    />
+                                    reverse
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.brody}
+                                        onChange={this.props.brodyChangeHandler}
+                                    />
+                                    brody
+                                </label>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.hueRotate}
+                                        onChange={this.props.hueRotateChangeHandler}
+                                    />
+                                    hue shift
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.invertColors}
+                                        onChange={this.props.invertColorsChangeHandler}
+                                    />
+                                    invert colors
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={this.props.emoteObject.slide}
+                                        onChange={this.props.slideChangeHandler}
+                                    />
+                                    slide
+                                </label>
+                                <select 
+                                    defaultValue={this.props.emoteObject.speed} 
+                                    onChange={this.props.speedChangeHandler}
+                                >
+                                    <option value="">-- optional slide speed modifier --</option>
+                                    <option value="slowest">slowest</option>
+                                    <option value="slower">slower</option>
+                                    <option value="slow">slow</option>
+                                    <option value="fast">fast</option>
+                                    <option value="faster">faster</option>
+                                    <option value="fastest">fastest</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="spin">Spin</label>
+                        <div className="col-md-8">
+                            <select
+                                className="form-control"
+                                id="spin"
+                                name="spin"
+                                defaultValue={this.props.emoteObject.spin}
+                                onChange={this.props.spinChangeHandler}
+                            >
+                                <option value="">-- optional spin --</option>
+                                <option value="xspin">spin clockwise around x axis</option>
+                                <option value="yspin">spin clockwise around y axis</option>
+                                <option value="zspin">spin clockwise around z axis</option>
+                                <option value="spin">spin clockwise around all 3 axes</option>
+                                <option value="!xspin">spin counterclockwise around x axis</option>
+                                <option value="!yspin">spin counterclockwise around y axis</option>
+                                <option value="!zspin">spin counterclockwise around z axis</option>
+                                <option value="!spin">spin counterclockwise around all 3 axes</option>
+                            </select>
+                        </div >
+                    </div >
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="rotateDegrees">Rotate (0 to 359)</label>
+                        <div className="col-md-8">
+                            <div className="input-group">
+                                <input
+                                    min="0"
+                                    max="359"
+                                    className="form-control"
+                                    id="rotateDegrees"
+                                    name="rotateDegrees"
+                                    type="number"
+                                    value={this.props.emoteObject.rotateDegrees.toString()}
+                                    onChange={this.props.rotateDegreesChangeHandler}
+                                    placeholder="degrees to rotate"
+                                />
+                                <span className="input-group-addon">degrees</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="xAxisTranspose">
+                            x axis shift (-150 to 150)
+                        </label>
+                        <div className="col-md-8">
+                            <div className="input-group">
+                                <input
+                                    min="-150"
+                                    max="150"
+                                    className="form-control"
+                                    id="xAxisTranspose"
+                                    name="xAxisTranspose"
+                                    type="number"
+                                    value={this.props.emoteObject.xAxisTranspose.toString()}
+                                    onChange={this.props.xAxisTransposeChangeHandler}
+                                    placeholder="x axis shift"
+                                />
+                                <span className="input-group-addon">pixels</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-md-4 control-label" htmlFor="zAxisTranspose">z index (0 to 10)</label>
+                        <div className="col-md-8">
+                            <input
+                                min="0"
+                                max="10"
+                                className="form-control"
+                                id="zAxisTranspose"
+                                name="zAxisTranspose"
+                                type="number"
+                                value={this.props.emoteObject.zAxisTranspose.toString()}
+                                onChange={this.props.zAxisTransposeChangeHandler}
+                                placeholder="z index"
+                            />
+                        </div>
+                    </div>
+                </fieldset >
+            </form >
         );
     }
 }
