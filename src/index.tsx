41d5078aecdd9fc227ffe1751ec5b2790e9c050b
James Manning <james.manning@gmail.com>
2018-04-05 15:36:29 -0400

    reorder imports so component css overrides bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <App />,
